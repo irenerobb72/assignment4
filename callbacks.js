29263b35aba9aeb7358ad98e74bc2665a1902d03
Irene <irene@Sugarat-4.local>
2019-02-25 23:13:43 -0500

    work

--- callbacks.js
+++ callbacks.js
@@ -1,37 +1,37 @@
-function isVowel (value) {
-    vowels = 'aeiouAEIOU'
-    // if the value is a single letter string and a vowel
-    if(value.length == 1 && vowels.includes(value) ){
-        return true
-    } else {
-        return false
+    function isVowel (value) {
+        vowels = 'aeiouAEIOU'
+        // if the value is a single letter string and a vowel
+        if(value.length == 1 && vowels.includes(value) ){
+            return true
+        } else {
+            return false
+        }
     }
-}
 
-console.log(isVowel('a'))
-console.log(isVowel('basd'))
-console.log(isVowel(2))
+    console.log(isVowel('a'))
+    console.log(isVowel('basd'))
+    console.log(isVowel(2))
 
-function countValues (value) {
-    letterList = [];
-	vowelCount = 0;
-	for (let i = 0; i < value.length; i++){
-		letterList.push(value[i].split());
-	}
-	vowelList = letterList.filter(isVowel);
-	for (let j = 0; j < vowelList.length; j++){
-		vowelCount++;
-	}
-	return vowelCount;
-}
+    function countValues (value) {
+        letterList = [];
+        vowelCount = 0;
+        for (let i = 0; i < value.length; i++){
+            letterList.push(value[i].split());
+        }
+        vowelList = letterList.filter(isVowel);
+        for (let j = 0; j < vowelList.length; j++){
+            vowelCount++;
+        }
+        return vowelCount;
+    }
 
-console.log(countValues('asdsdgoek'))
-console.log(countValues('abcdefghijklmnopqrstuvwxyz'))
-console.log(countValues('aeiou'))
-console.log(countValues('aaaaaaaaaaa'))
+    console.log(countValues('asdsdgoek'))
+    console.log(countValues('abcdefghijklmnopqrstuvwxyz'))
+    console.log(countValues('aeiou'))
+    console.log(countValues('aaaaaaaaaaa'))
 
 
-function byVowelCount (array) {
-    return array.sort()
-}
-console.log(byVowelCount())
+    function byVowelCount (array) {
+        return array.sort()
+    }
+    console.log(byVowelCount())

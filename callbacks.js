303f88bf96c5748bc989ceebfeef13936cba7575
Irene <irene@Sugarat-4.local>
2019-02-26 11:24:34 -0500

    new changes

--- callbacks.js
+++ callbacks.js
@@ -1,11 +1,7 @@
     function isVowel (value) {
-        vowels = 'aeiouAEIOU'
+        const VOWELS = ['a','e','i','o','u'];
         // if the value is a single letter string and a vowel
-        if(value.length == 1 && vowels.includes(value) ){
-            return true
-        } else {
-            return false
-        }
+        return value.length === 1 && VOWELS.includes(value) 
     }
 
     console.log(isVowel('a'))
@@ -13,16 +9,9 @@
     console.log(isVowel(2))
 
     function countValues (value) {
-        letterList = [];
-        vowelCount = 0;
-        for (let i = 0; i < value.length; i++){
-            letterList.push(value[i].split());
-        }
-        vowelList = letterList.filter(isVowel);
-        for (let j = 0; j < vowelList.length; j++){
-            vowelCount++;
-        }
-        return vowelCount;
+        // const letters = value.split('');
+        return value.split('').filter(isVowel);
+        // possibly add a .dot length at the end?
     }
 
     console.log(countValues('asdsdgoek'))
@@ -31,49 +20,63 @@
     console.log(countValues('aaaaaaaaaaa'))
 
 
+
     function byVowelCount (array) {
-        return array.sort()
+        // compare 2 strings, if string has more vowels sort it first
+        return array.sort((a,b) => countValues(a) > countValues(b));
+
     }
-    console.log(byVowelCount())
+    console.log(byVowelCount([
+        "aardvark",
+        "orangutan",
+        "chinchilla",
+        "pika",
+        "mongoose",
+        "komodo",
+        "pangolin",
+        "emu",
+        "penguin",
+        "narwhal",
+        "manatee"]))
 
 
-    // closuress
-    const nextFibonacci = (function () {
-        let x = 1, y = 1;
-        return function () {
-            let hold = x;
-            x = y;
-            y = hold + y;
-            return hold;
-        }
-    }());
+    // // closuress
+    // const nextFibonacci = (function () {
+    //     let x = 1, y = 1;
+    //     return function () {
+    //         let hold = x;
+    //         x = y;
+    //         y = hold + y;
+    //         return hold;
+    //     }
+    // }());
     
-    console.log(nextFibonacci())
-    console.log(nextFibonacci())
-    console.log(nextFibonacci())
-    console.log(nextFibonacci())
-    console.log(nextFibonacci())
-    console.log(nextFibonacci())
-    console.log(nextFibonacci())
-    console.log(nextFibonacci())
-    console.log(nextFibonacci())
-    console.log(nextFibonacci())
+    // console.log(nextFibonacci())
+    // console.log(nextFibonacci())
+    // console.log(nextFibonacci())
+    // console.log(nextFibonacci())
+    // console.log(nextFibonacci())
+    // console.log(nextFibonacci())
+    // console.log(nextFibonacci())
+    // console.log(nextFibonacci())
+    // console.log(nextFibonacci())
+    // console.log(nextFibonacci())
     
     
-    function runningAverage(value) {
-        if (isNaN(value)) {
-             this.count = 0
-             this.sum = 0
-             return NaN
-        } 
-        // if this has not been called set it to 0 
-        this.count = (this.count || 0) + 1
-        this.sum = (this.sum || 0) + value
-        return sum/count
-    }
+    // function runningAverage(value) {
+    //     if (isNaN(value)) {
+    //          this.count = 0
+    //          this.sum = 0
+    //          return NaN
+    //     } 
+    //     // if this has not been called set it to 0 
+    //     this.count = (this.count || 0) + 1
+    //     this.sum = (this.sum || 0) + value
+    //     return sum/count
+    // }
     
-    const runningAverage2 = (function () {
+    // const runningAverage2 = (function () {
         
         
-    }());
+    // }());
     

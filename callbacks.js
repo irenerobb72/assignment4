2b4fdc6ea96d53de0c640228686629c45fabe85b
Irene <irene@Sugarat-4.local>
2019-02-27 16:02:29 -0500

    hi:

--- callbacks.js
+++ callbacks.js
@@ -8,59 +8,58 @@
     console.log(isVowel('basd'))
     console.log(isVowel(2))
 
-    function countValues (value) {
-        // const letters = value.split('');
+    function countVowels (value) {
         return value.split('').filter(isVowel);
         // possibly add a .dot length at the end?
     }
 
-    console.log(countValues('asdsdgoek'))
-    console.log(countValues('abcdefghijklmnopqrstuvwxyz'))
-    console.log(countValues('aeiou'))
-    console.log(countValues('aaaaaaaaaaa'))
+    console.log(countVowels('asdsdgoek'))
+    console.log(countVowels('abcdefghijklmnopqrstuvwxyz'))
+    console.log(countVowels('aeiou'))
+    console.log(countVowels('aaaaaaaaaaa'))
 
 
 
     function byVowelCount (array) {
         // compare 2 strings, if string has more vowels sort it first
-        return array.sort((a,b) => countValues(a) > countValues(b));
+        return array.sort((a,b) => countVowels(a) > countVowels(b));
 
     }
-    console.log(byVowelCount([
-        "aardvark",
-        "orangutan",
-        "chinchilla",
-        "pika",
-        "mongoose",
-        "komodo",
-        "pangolin",
-        "emu",
-        "penguin",
-        "narwhal",
-        "manatee"]))
+    // console.log(byVowelCount([
+    //     "aardvark",
+    //     "orangutan",
+    //     "chinchilla",
+    //     "pika",
+    //     "mongoose",
+    //     "komodo",
+    //     "pangolin",
+    //     "emu",
+    //     "penguin",
+    //     "narwhal",
+    //     "manatee"]))
 
 
-    // // closuress
-    // const nextFibonacci = (function () {
-    //     let x = 1, y = 1;
-    //     return function () {
-    //         let hold = x;
-    //         x = y;
-    //         y = hold + y;
-    //         return hold;
-    //     }
-    // }());
+    // closuress
+    const nextFibonacci = (function () {
+        let x = 1, y = 1;
+        return function () {
+            let hold = x;
+            x = y;
+            y = hold + y;
+            return hold;
+        }
+    }());
     
-    // console.log(nextFibonacci())
-    // console.log(nextFibonacci())
-    // console.log(nextFibonacci())
-    // console.log(nextFibonacci())
-    // console.log(nextFibonacci())
-    // console.log(nextFibonacci())
-    // console.log(nextFibonacci())
-    // console.log(nextFibonacci())
-    // console.log(nextFibonacci())
-    // console.log(nextFibonacci())
+    console.log(nextFibonacci())
+    console.log(nextFibonacci())
+    console.log(nextFibonacci())
+    console.log(nextFibonacci())
+    console.log(nextFibonacci())
+    console.log(nextFibonacci())
+    console.log(nextFibonacci())
+    console.log(nextFibonacci())
+    console.log(nextFibonacci())
+    console.log(nextFibonacci())
     
     
     // function runningAverage(value) {
@@ -75,8 +74,19 @@
     //     return sum/count
     // }
     
-    // const runningAverage2 = (function () {
-        
-        
-    // }());
-    
+    const runningAverage2 = (function (value) {
+        console.log(value)
+        return function(value) {
+            console.log('hello')
+            if (isNaN(value)) {
+                count = 0
+                sum = 0
+                return NaN
+            } 
+            count = (this.count || 0) + 1
+            sum = (this.sum || 0) + 1
+            return sum / count
+        }
+    }());
+    console.log('hi')
+    console.log(runningAverage2(300))
